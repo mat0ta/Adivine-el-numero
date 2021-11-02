1264e5eb51a7c2c0ccbe685d3609c54282eaf622
Matota <72810000+mat0ta@users.noreply.github.com>
2021-11-02 10:49:54 +0100

    Update adivina_el_numero.py

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,8 +1,20 @@
 # Imports
+from itertools import permutations
 import random
 # Declaramos las variables
 number = random.randint(0,99)
+intentos = 0
 
+# Main Code
 givenNumber = input('Adivina el número: ')
 while not givenNumber.isdigit():
     givenNumber = input('Introduzca un número válido: ')
+if givenNumber != number:
+    if givenNumber > number:
+        intentos =+ 1
+        print('No has acertado, te has pasado.\nYa llevas ' + str(intentos) + ' intentos.')
+    elif givenNumber < number:
+        intentos =+ 1
+        print('No has acertado, te has quedado corto.\nYa llevas ' + str(intentos) + ' intentos.')
+if givenNumber == number:
+    print('Has acertado! Lo ha conseguido en ' + str(intentos) + ' intentos.')
